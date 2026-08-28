1b6632f1eb56d0436aa3d7eccb646be973f7e36b
utsav1103 <utsavu858@gmail.com>
2026-08-28 22:34:08 +0530

    using inputs

--- variables/ex2.py
+++ variables/ex2.py
@@ -1,5 +1,8 @@
 name = input("Please enter your name:")
 color = input("what is your favorite color?:")
-
+birth = input("plese enter your birth year: ")
 print ("Hello " + name +" welcome to battalion")
 print ("your favorite color is " + color)
+
+print (name + " likes " + color + " color")
+print (name + " was born in " + birth)
