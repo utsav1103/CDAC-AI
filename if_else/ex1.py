48795b2c09dea2fd690a47df4b77ba5c7b7f75cb
utsav1103 <utsavu858@gmail.com>
2026-08-30 19:01:37 +0530

    if else

---
+++ if_else/ex1.py
@@ -0,0 +1,16 @@
+def main():
+    is_hot = False
+    is_cold = False
+    is_ok = True
+
+    if is_hot:
+        print("It's a hot day!Drink plenty of water")
+
+    elif is_ok:
+        print("It's a nice day! Wear light clothes")
+        
+    else:
+        print("It's a cold day! Wear warm clothes")    
+    print("Enjoy your day!")        
+
+main()    
