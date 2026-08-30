1c91d33bde06e317622f864067af7180af2a782d
utsav1103 <utsavu858@gmail.com>
2026-08-30 18:14:54 +0530

    using len fucntion

--- strings/ex1.py
+++ strings/ex1.py
@@ -4,3 +4,13 @@ last = 'Chan'
 full_name = f"{first}-{last}"
 
 print (full_name)
+
+
+print("Hey! cutie " * 7)
+
+# inbulid len function
+
+name = "Gitthal Chan"
+print(name)
+
+print(f"Your name has {len(name)} characters.")
