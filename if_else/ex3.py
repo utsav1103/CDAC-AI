739b637e32ec2af81e550d3e67c40369b06c6fa2
utsav1103 <utsavu858@gmail.com>
2026-08-30 21:00:32 +0530

    comparison operator

---
+++ if_else/ex3.py
@@ -0,0 +1,15 @@
+def main():
+
+
+    tem = int(input("Enter the temperature :"))
+
+    if tem > 30:
+        print("it's a hot day :)")
+
+    elif tem < 10:
+        print("it's a cold day :(")
+
+    else:
+        print("it's a nice day :)")        
+
+main()
