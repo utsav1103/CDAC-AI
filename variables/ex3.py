43ca8349793a6a8ade7c201afe29168d0af93837
utsav1103 <utsavu858@gmail.com>
2026-08-30 12:17:04 +0530

    kg_to_lbs

---
+++ variables/ex3.py
@@ -0,0 +1,12 @@
+# birth_year = input("Enter your birth year: ")
+# current_year = 2026
+# print(type(birth_year))
+# age = current_year - int(birth_year)
+
+# print (f"Gitthal is {age} years young.))")
+
+weight = int (input("enter your weight in kg:"))
+
+to_pounds = weight * 2.20
+
+print (f"your weight in pounds is {to_pounds} lbs.")
