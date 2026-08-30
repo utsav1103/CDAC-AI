f106d6a60ea5557492b2f46aae6a003adf7319e2
utsav1103 <utsavu858@gmail.com>
2026-08-30 20:54:02 +0530

    question if else

---
+++ if_else/ex2.py
@@ -0,0 +1,13 @@
+def main():
+
+    price = int (input("Enter the price of the House :"))
+
+    if price > 1000000:
+        print("The house is too expensive, you need to put down 10% of the price")
+
+    else:
+        print("The house is not too expensive, you need to put down 20% of the price")
+
+
+
+main()    
