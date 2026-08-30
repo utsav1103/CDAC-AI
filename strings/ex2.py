a3e3ac85bdae4cb409b68f721a490eeb69c3e9c4
utsav1103 <utsavu858@gmail.com>
2026-08-30 18:50:06 +0530

    math functions

--- strings/ex2.py
+++ strings/ex2.py
@@ -1,3 +1,5 @@
+import math
+
 print(10/3)
 print(10//3)
 print(10%3)
@@ -16,3 +18,15 @@ print(x)
 # ? parentheses > exponential operator > multiplication > division > addition > subtraction
 x = (10 + 3) * 2 ** 2
 print(x)
+
+#* math functions
+
+x = 2.9
+print(round(x))
+
+x = -2.9
+print(abs(x))
+
+x = 3.6
+print(math.ceil(x))  # rounds up to the nearest integer
+print(math.floor(x))  # rounds down to the nearest integer
