4c9ab8fb1bc166cb8006bec70877ad4633b6e925
utsav1103 <utsavu858@gmail.com>
2026-08-30 12:19:27 +0530

    lbs_to_kg

--- variables/ex3.py
+++ variables/ex3.py
@@ -9,4 +9,9 @@ weight = int (input("enter your weight in kg:"))
 
 to_pounds = weight * 2.20
 
+weight2 = int(input("enter your weight in lbs:"))
+
+to_kg = weight2 * 0.45
+
 print (f"your weight in pounds is {to_pounds} lbs.")
+print (f"your weight in kg is {to_kg} kg.")
