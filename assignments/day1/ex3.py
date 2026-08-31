12112387fad580662bbfdf5e269dc2d1f0c2c7ec
utsav1103 <utsavu858@gmail.com>
2026-08-31 22:20:18 +0530

    prime number

---
+++ assignments/day1/ex3.py
@@ -0,0 +1,13 @@
+def prime():
+
+    num = int(input("Enter a number:"))
+
+    if num > 2:
+        for i in range(2,num):
+            if num % i == 0:
+                print(num, "is not a prime number")
+                break
+        else:
+            print(num, "is a prime number")
+
+prime()    
