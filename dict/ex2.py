3c393b5a171790febde6a23a5226cfed1f5c67a7
utsav1103 <utsavu858@gmail.com>
2026-09-06 19:27:50 +0530

    phone number to string

---
+++ dict/ex2.py
@@ -0,0 +1,21 @@
+contact = input("Enter contact number : ")
+
+#mapping
+digit_map = {
+    "1": "One",
+    "2": "Two",
+    "3": "Three",
+    "4": "Four",
+    "5": "Five",
+    "6": "Six",
+    "7": "Seven",
+    "8": "Eight",
+    "9": "Nine",
+    "0": "Zero"
+}
+output = ""
+
+for ch in contact: 
+    output += digit_map.get(ch, "!") + " "
+
+print(output)
